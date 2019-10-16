4b2e55028fc80720e2da83d773c3cb266666a14b
LeshchenkoB <lba84@mail.ru>
2019-10-16 09:08:10 +0700

    вынес путь в переменную и добавил комментарии к коду

--- engine/dir.php
+++ engine/dir.php
@@ -1,12 +1,13 @@
 <?php
 
-function myScan(string $dir){
+$directory = "../public/img/small/"; // путь к папке с картинками
+function myScan($dir){
     $path = scandir($dir); // запишем все содержимое папки img/small
-    if (!$path){
+    if (!$path){           // если указанный путь не является каталогом или его нет, то scandir выведет false
         return false;
     }
-    unset($path[0], $path[1]);
+    unset($path[0], $path[1]); // удалим из массива служебные элементы "." и "..", которые функция scandir всегда записывает в первые два элемента массива
     return $path;
 }
 
-$dir=myScan("../public/img/small/");
+$dir=myScan($directory); // массив файлов в папке
