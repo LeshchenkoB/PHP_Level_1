4a03f699fe3433de4f995b5511047d386b375373
LeshchenkoB <lba84@mail.ru>
2019-10-16 09:54:01 +0700

    вынес путь в переменную и добавил комментарии к коду

--- engine/server.php
+++ engine/server.php
@@ -1,7 +1,7 @@
 <?php
-
+$directory = "../public/img/small/"; // путь к папке с картинками
 //print_r($_FILES);
-$pathPhoto = "../public/img/small/".$_FILES['photo']['name'];
+$pathPhoto = $directory.$_FILES['photo']['name']; // создаем путь, куда будут помещаться загружаемые картинки
 if (move_uploaded_file($_FILES['photo']['tmp_name'],$pathPhoto)){
     echo "Файл ".$_FILES['photo']['name']." успешно загружен! </br></br>";
 }
