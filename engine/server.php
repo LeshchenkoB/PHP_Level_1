4ff2e48610c60f3c8ca789880cb3b82c73c0cbb4
LeshchenkoB <lba84@mail.ru>
2019-10-16 20:10:49 +0700

    Сделал проверку на загружаемые файлы

--- engine/server.php
+++ engine/server.php
@@ -1,9 +1,13 @@
 <?php
 $directory = "../public/img/small/"; // путь к папке с картинками
 //print_r($_FILES);
-$pathPhoto = $directory.$_FILES['photo']['name']; // создаем путь, куда будут помещаться загружаемые картинки
-if (move_uploaded_file($_FILES['photo']['tmp_name'],$pathPhoto)){
-    echo "Файл ".$_FILES['photo']['name']." успешно загружен! </br></br>";
+if (in_array($_FILES['photo']['type'], array("image/png", "image/jpeg","image/pjpeg"))){ // если загружаемый файл картинка
+    $pathPhoto = $directory.$_FILES['photo']['name']; // создаем путь, куда будут помещаться загружаемые картинки
+    if (move_uploaded_file($_FILES['photo']['tmp_name'],$pathPhoto)){ // перемещаем загруженный файл в новое место
+        echo "Файл ".$_FILES['photo']['name']." успешно загружен! </br></br>";
+    }
 }
+else echo "Файл ".$_FILES['photo']['name']." не является картинкой! </br></br>";
+
 ?>
 <a href="../templates/index.php">Вернуться в галерею</a>
