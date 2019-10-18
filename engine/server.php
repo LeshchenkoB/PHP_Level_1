96200d5e14a0606b6c4b9d11ba043ae64f5a6d6b
LeshchenkoB <lba84@mail.ru>
2019-10-18 18:48:26 +0700

    Добавил загрузку выбранного фото в папку для больших фото

--- engine/server.php
+++ engine/server.php
@@ -2,25 +2,22 @@
 require "dir.php";
 require "../config/config.php";
 
-function sqlQueryIntoImg($path_big, $path_small,$connect){ // функция создает запров к БД (table = img) и отпраляет его
-    $sql = "insert into img(src_big, src_small) values ('$path_big', '$path_small')"; // команда для SQL для добавления данных
+function sqlQueryIntoImg($path_big, $path_small,$connect){ // функция создает запрос к БД (table = img) и отправляет его
+    $sql = "insert into img(src_big, src_small) values ('$path_big', '$path_small')"; // команда для SQL для добавления данных о путях к папке
     mysqli_query($connect, $sql); // отправляем запрос к БД
 }
-
 
 //print_r($_FILES)."</br>";
 if (in_array($_FILES['photo']['type'], array("image/png", "image/jpeg","image/pjpeg"))){ // если загружаемый файл картинка
     $path_big = $directoryBig_db.$_FILES['photo']['name']; // создаем путь, для больших картинок
     $path_small = $directorySmall_db.$_FILES['photo']['name']; // создаем путь, для малельних картинкок
+    $pathPhoto_big = $directoryBig.$_FILES['photo']['name']; // путь для складирования загружаемых файлов
 
-//echo $path_big."</br>";
-//echo $path_small."</br>";
-//echo $sql."</br>";
     sqlQueryIntoImg($path_big, $path_small, $connect);
 
-//    if (move_uploaded_file($_FILES['photo']['tmp_name'],$pathPhoto)){ // перемещаем загруженный файл в новое место
-//        echo "Файл ".$_FILES['photo']['name']." успешно загружен! </br></br>";
-//    }
+    if (move_uploaded_file($_FILES['photo']['tmp_name'],$pathPhoto_big)){ // перемещаем загруженный файл в новое место
+        echo "Файл ".$_FILES['photo']['name']." успешно загружен! </br></br>";
+    }
 }
 else echo "Файл ".$_FILES['photo']['name']." не является картинкой! </br></br>";
 
